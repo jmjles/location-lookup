81c14ba5745c6b6a3e3ab2c4433659889aeb7def
jmjles <jmjles@gmail.com>
2020-02-16 16:59:55 -0800

    Added and Completed Express Server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const KeyRoute = require('./routes/keys')
+
+require('dotenv').config()
+
+const port = process.env.PORT
+
+const middleware = [express.json()]
+
+app.use(middleware)
+app.use('/',KeyRoute)
+
+app.listen(port,()=> console.log(`\nServer is running on port:${port}`))
